ea14f277267a1e3dd35213f9966916c17b633192
Muthuganesan N G <96805495+Muthuganesan2003@users.noreply.github.com>
2023-05-20 21:40:43 +0530

    rday6

---
+++ SIGNMOVE.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;cin>>T;
+	while(T--){
+	    int n;cin>>n;
+	    if(n%2==0)cout<<n/2<<endl;
+	    else cout<<-1*((n+1)/2)<<endl;
+	   
+	}
+	return 0;
+}

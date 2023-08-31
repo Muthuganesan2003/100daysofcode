11db66733f5e8abf1b1be05c528f651f19f995cd
Muthuganesan N G <96805495+Muthuganesan2003@users.noreply.github.com>
2023-08-31 21:04:05 +0530

    Create Find the Index of the First Occurrence in a String.cpp
    Leetcode

---
+++ Find the Index of the First Occurrence in a String.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        for(int i=0;i<haystack.size();i++){
+            if(haystack.substr(i,needle.size())== needle)return i;
+        }
+        return -1;
+    }
+};
